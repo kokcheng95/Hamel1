435b75aa00defea0d6f8efa9c8d861d76be8f2fb
kokcheng95 <33620197+kokcheng95@users.noreply.github.com>
2018-01-10 20:44:55 +0800

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ if(isset($_REQUEST['hub_challenge'])) {
  $challenge = $_REQUEST['hub_challenge'];
  $token = $_REQUEST['hub_verify_token'];
  
- if($token == "myCustomToken123") {
+ if($token == "kokcheng123") {
  echo $challenge;
  }
 }
