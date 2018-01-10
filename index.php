65cedb72ace4c6ffd003c0585262b68260e5f427
kokcheng95 <33620197+kokcheng95@users.noreply.github.com>
2018-01-10 20:18:04 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,5 @@
 <?php
 
-if(isset($_REQUEST['hub_challenge'])) {
- $challenge = $_REQUEST['hub_challenge'];
- $token = $_REQUEST['hub_verify_token'];
- 
- if($token == "kokcheng123") {
- echo $challenge;
- }
-}
+echo "Hello World";
 
 ?>﻿
